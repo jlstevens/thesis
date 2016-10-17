29cc344058d2d4361224e8da2d45b8d4d3cb8adc
jlstevens <jlstevens@ed.ac.uk>
2016-10-17 12:14:32 +0100

    Registered plugins in pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,14 @@ PATH = 'content'
 
 THEME='./pelican-mockingbird' # pelican-alchemy was considered
 
+PLUGIN_PATHS = ['./pelican-plugins'] # Clone of the repository
+PLUGINS = ['summary',                # Allows breaks
+           'liquid_tags.youtube',    # May be useful e.g: {% youtube lEEa83Tsjrg %}
+           'liquid_tags.notebook',   # Useful!
+           'liquid_tags.nbinlined',  # My own additional tag
+           'pelican_dynamic']        # See https://github.com/wrobstory/pelican_dynamic.git
+
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None

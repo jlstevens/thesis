2a010be691eb00b0b0ed63b55a7db14fb8b7992b
jlstevens <jlstevens@ed.ac.uk>
2016-10-19 22:28:50 +0100

    Completed setup.py for stevens_thesis16 package

--- setup.py
+++ setup.py
@@ -5,3 +5,22 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+setup_args = {}
+setup_args.update(dict(
+    name='stevens_thesis16',
+    version="1.0",
+    description='',
+    long_description=open('README.rst').read() if os.path.isfile('README.rst') else 'Consult README.rst',
+    author= "jlstevens",
+    author_email= "jstevens@continuum.io",
+    maintainer= "jlstevens",
+    maintainer_email= "jlstevens@continuum.io",
+    platforms=['Windows', 'Mac OS X', 'Linux'],
+    packages = ["stevens_thesis16"],
+    provides = ["stevens_thesis16"],
+))
+
+
+if __name__=="__main__":
+    setup(**setup_args)
